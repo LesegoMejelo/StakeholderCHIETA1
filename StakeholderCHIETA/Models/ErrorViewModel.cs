f0623b38d9d24b38518eba68bcaad62dc25afd6b
LesegoMejelo <MJLLES003@myuct.ac.za>
2025-06-20 12:24:35 +0200

    Add project files.

---
+++ StakeholderCHIETA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StakeholderCHIETA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
