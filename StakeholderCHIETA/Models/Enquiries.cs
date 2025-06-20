9645129bff745cce818d38bb5e587d227e0db46d
Lilitha Kunene <KNNLIL002@myuct.ac.za>
2025-06-20 13:33:47 +0200

    newer change

--- StakeholderCHIETA/Models/Enquiries.cs
+++ StakeholderCHIETA/Models/Enquiries.cs
@@ -1,7 +1,12 @@
-﻿namespace StakeholderCHIETA.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StakeholderCHIETA.Models
 {
+    [Table("Enquiry")]
     public class Enquiries
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EnquiryId { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
