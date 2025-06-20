4db97e32e7f28c618ba8a72aa7236a9352f7a4e9
Lilitha Kunene <KNNLIL002@myuct.ac.za>
2025-06-20 14:12:18 +0200

    new

--- StakeholderCHIETA/Models/EnquiryStatus.cs
+++ StakeholderCHIETA/Models/EnquiryStatus.cs
@@ -3,18 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StakeholderCHIETA.Models
 {
-    public enum Status
-    {
-        Pending,
-        InProgress,
-        Resolved
-    }
+    
     public class EnquiryStatus
     {
+        public enum Status
+        {
+            Pending,
+            InProgress,
+            Resolved
+        }
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StatusId { get; set; }
-        
         public string status {  get; set; }
-        public ICollection<Enquiries> Enquiry { get; set; }
+       // public ICollection<Enquiries> Enquiry { get; set; }
     }
 }
