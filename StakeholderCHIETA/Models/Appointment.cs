eae1c82491a761d53bca79ecf7a365bbec87515d
LesegoMejelo <MJLLES003@myuct.ac.za>
2025-08-14 20:01:46 +0200

    Appointment Controller and model

---
+++ StakeholderCHIETA/Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿namespace StakeholderCHIETA.Models
+{
+    public class Appointment
+    {
+        public string AppointmentId { get; set; }
+        public string ClientId { get; set; }
+        public string AdvisorId { get; set; }
+        public DateTime Date { get; set; }
+        public string Purpose { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
---
+++ StakeholderCHIETA/Models/AppointmentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace StakeholderCHIETA.Models
+{
+    public class AppointmentViewModel
+    {
+        [Required]
+        [Display(Name = "Advisor")]
+        public string AdvisorId { get; set; }
+        public List<SelectListItem> Advisors { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Reason { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan Time { get; set; }
+    }
+}
