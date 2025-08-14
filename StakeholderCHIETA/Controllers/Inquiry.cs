8ec0b1609ccbe0af9a583ca0b81bedd7e478958c
lilitha-kunene <knnlil002@myuct.ac.za>
2025-08-14 19:51:39 +0200

    fingers crosed

--- StakeholderCHIETA/Controllers/Inquiry.cs
+++ StakeholderCHIETA/Controllers/Inquiry.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Staekholder_CHIETA_X.Controllers
 {
+
+
     public class InquiryController : Controller
     {
+        private readonly FirestoreDb _db;
+
+        public InquiryController(FirestoreDb db)
+        {
+            _db = db;
+        }
+
+
+        [HttpPost]
+        [Route("api/inquiry")]
+        public async Task<IActionResult> Post([FromForm] string name, [FromForm] string message, [FromForm] string inquiryType)
+        {
+            var docRef = await _db.Collection("inquiries").AddAsync(new
+            {
+                name = name,
+                message = message,
+                inquiryType = inquiryType,
+                createdAt = Timestamp.GetCurrentTimestamp()
+            });
+
+            return Ok(new { id = docRef.Id, message = "Inquiry submitted" });
+        }
         public IActionResult Index()
         {
             return View();
