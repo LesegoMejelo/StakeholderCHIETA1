2fa804e94426e871a3364d3b80f5de889cc47b5e
LesegoMejelo <MJLLES003@myuct.ac.za>
2025-08-18 20:29:32 +0200

    mmh

--- StakeholderCHIETA/Models/Appointment.cs
+++ StakeholderCHIETA/Models/Appointment.cs
@@ -4,6 +4,7 @@
     {
         public string AppointmentId { get; set; }
         public string ClientId { get; set; }
+        public string ClientName { get; set; }
         public string AdvisorId { get; set; }
         public DateTime Date { get; set; }
         public string Purpose { get; set; }
