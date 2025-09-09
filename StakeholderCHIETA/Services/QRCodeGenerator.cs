2d7e35dc1bca9770465e645c9b8c3814e395dd4e
OnthatileMMbowane <MBWONT001@myuct.ac.za>
2025-09-09 16:07:44 +0200

    updated stakeholder - Risky

---
+++ StakeholderCHIETA/Services/QRCodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿/* using QRCoder;
+
+namespace StakeholderCHIETA.Services
+{
+    public class QRCodeService : IQRCodeGenerator
+    {
+        public async Task<byte[]> GenerateQRCodeAsync(string data)
+        {
+            using var qrGenerator = new QRCoder.QRCodeGenerator(); // explicitly use QRCoder’s class
+            var qrCodeData = qrGenerator.CreateQrCode(data, QRCoder.QRCodeGenerator.ECCLevel.Q);
+
+            using var qrCode = new PngByteQRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
+        }
+    }
+}
+*/
