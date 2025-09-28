ef41e2394f450c2853b0c87c575bb675483653f1
OnthatileMMbowane <MBWONT001@myuct.ac.za>
2025-09-28 17:04:12 +0200

    Ratings controller

---
+++ StakeholderCHIETA/Controllers/Ratings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StakeholderCHIETA.Controllers
+{
+    public class Ratings : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
