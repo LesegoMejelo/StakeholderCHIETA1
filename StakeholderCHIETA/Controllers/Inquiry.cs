3038f1a53d2069412bd8109455da1aa7378c48be
lilitha-kunene <knnlil002@myuct.ac.za>
2025-09-29 11:41:25 +0200

    idk

--- StakeholderCHIETA/Controllers/Inquiry.cs
+++ StakeholderCHIETA/Controllers/Inquiry.cs
@@ -411,5 +411,7 @@ namespace Staekholder_CHIETA_X.Controllers
         public IActionResult Index() => View();
         public IActionResult Inquiry() => View("~/Views/StakeholderViews/Inquiry/Inquiry.cshtml");
         public IActionResult Tracking() => View("~/Views/EmployeeViews/InquiryTracker.cshtml");
+
+        public IActionResult InquiryTracking() => View("~/Views/StakeholderViews/InquiryTracking/Inquiry.cshtml");
     }
 }
